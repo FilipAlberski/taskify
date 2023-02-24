7733f2a86df7875a75ee5335b4561f13bde61809
Filip Alberski <alberskif@gmail.com>
2023-02-24 19:27:00 +0100

    creating grid pattern on landing page

--- client/src/pages/landing/Landing.jsx
+++ client/src/pages/landing/Landing.jsx
@@ -1,6 +1,21 @@
+import Logo from "../../components/Logo/Logo";
 import { LandingContainer } from "./Landing.styled";
 
 const Landing = () => {
-    return <LandingContainer>teeest</LandingContainer>;
+    return (
+        <LandingContainer>
+            <div className="topPart">
+                <Logo />
+            </div>
+            <div className="bottomPart">
+                <div className="left">
+                    <h1>Taskify</h1>
+                </div>
+                <div className="right">
+                    <h1>Taskify</h1>
+                </div>
+            </div>
+        </LandingContainer>
+    );
 };
 export default Landing;
--- client/src/pages/landing/Landing.styled.js
+++ client/src/pages/landing/Landing.styled.js
@@ -3,10 +3,45 @@ import styled from "styled-components";
 export const LandingContainer = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
+
     height: 100vh;
     width: 100vw;
-    background: #f5f5f5;
-    color: var(--primary-color);
+    background: var(--primary-color3);
+
+    color: var(--font-color);
+
+    .topPart {
+        height: 80px;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        padding: 0 1.75rem;
+    }
+    .bottomPart {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr;
+
+        .left {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding: 0 1.75rem;
+        }
+
+        .right {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding: 0 1.75rem;
+        }
+
+        @media (max-width: 768px) {
+            grid-template-columns: 1fr;
+            grid-template-rows: 1fr 1fr;
+        }
+    }
 `;
