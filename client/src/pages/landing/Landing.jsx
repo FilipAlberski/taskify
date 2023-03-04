6f38de7e91eaec26fb731c279948f75710491a0d
Filip Alberski <alberskif@gmail.com>
2023-03-04 21:51:14 +0100

    git test

--- client/src/pages/landing/Landing.jsx
+++ client/src/pages/landing/Landing.jsx
@@ -2,7 +2,7 @@ import Logo from "../../components/Logo/Logo";
 import { LandingContainer } from "./Landing.styled";
 import img from "../../assets/landingimg.svg";
 const Landing = () => {
-    return <LandingContainer>landing</LandingContainer>;
+    return <LandingContainer>landing test</LandingContainer>;
 };
 
 export default Landing;
