a2b70b5f299039100e2e2ca7bfc55e43b2e784e0
Filip Alberski <alberskif@gmail.com>
2023-03-16 21:10:49 +0100

    adding console after connection with mongodb

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -2,10 +2,17 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const connectDB = async (url) => {
-    mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    mongoose
+        .connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
+        .catch((err) => {
+            console.log("Error connecting to MongoDB", err);
+        });
 };
 
 module.exports = connectDB;
