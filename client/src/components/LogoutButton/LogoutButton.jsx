4e8bf645c23b3f55774fe6c9dd3938dee5810ba6
Filip Alberski <alberskif@gmail.com>
2023-04-15 18:43:23 +0200

    adding fuctionality to logout on front

--- client/src/components/LogoutButton/LogoutButton.jsx
+++ client/src/components/LogoutButton/LogoutButton.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { StyledLogoutButton } from "./LogoutButton.styled.js";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 const LogoutButton = () => {
-    const handleLogout = () => {};
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        try {
+            axios.get("/api/auth/logout");
+            navigate("/landing");
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-    return <StyledLogoutButton>Logout</StyledLogoutButton>;
+    return (
+        <StyledLogoutButton onClick={handleLogout}>Logout</StyledLogoutButton>
+    );
 };
 
 export default LogoutButton;
