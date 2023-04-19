b8673b64261f4429a490e6537e274ab51eb92572
Filip Alberski <alberskif@gmail.com>
2023-04-19 18:50:40 +0200

    working show premitted tasks!!!!

--- server/controllers/utils/checkPermission.js
+++ server/controllers/utils/checkPermission.js
@@ -1,12 +1,17 @@
 function checkPermission(user, project, task) {
     const isSuperadmin = user.roles.includes("superAdmin");
 
-    //check if user is member of project by his id in members array but this is not working
-    const isMember = user._id in project.members;
+    //check if user is member of project by his id in members array
+    const isMember = project.members.some((member) => {
+        return member.equals(user._id);
+    });
 
-    const isCoworker = user._id in task.coworkers;
+    //check if user is coworker of task by his id in coworkers array
 
-    //if user is super admin or member of project or coworker of task
+    const isCoworker = task.coworkers.some((coworker) => {
+        return coworker.equals(user._id);
+    });
+
     if (isSuperadmin || isMember || isCoworker) {
         return true;
     }
