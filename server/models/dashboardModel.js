7dfb0d752725b730a6941f817c3b9d9fdfcf5a40
Filip Alberski <alberskif@gmail.com>
2023-04-22 19:48:36 +0200

    creating dashboard model

---
+++ server/models/dashboardModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const { schema } = mongoose;
+
+const dashboardSchema = new schema({
+    name: {
+        type: String,
+        required: true,
+    },
+
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    isPrivate: {
+        type: Boolean,
+        default: false,
+        required: true,
+    },
+    querySettings: {
+        type: Object,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        required: true,
+        immutable: true,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+});
