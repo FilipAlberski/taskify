9ca3b323cca0ce0b9e92a813b74e945c48b62619
Filip Alberski <alberskif@gmail.com>
2023-07-19 09:24:56 +0200

    setting up register page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,148 @@
 import React from 'react';
 
+import {
+  Container,
+  Avatar,
+  Typography,
+  Box,
+  Grid,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Link,
+  CssBaseline,
+} from '@mui/material';
+
+import { useState } from 'react';
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const [registerData, setRegisterData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    acceptRules: false,
+    allowExtraEmails: false,
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event) => {
+    const { name, value, checked } = event.target;
+    setRegisterData((prev) => ({
+      ...prev,
+      [name]: name === 'acceptRules' ? checked : value,
+    }));
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>essa</Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box
+          component="form"
+          noValidate
+          onSubmit={handleSubmit}
+          sx={{ mt: 3 }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="given-name"
+                name="firstName"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="family-name"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    value={registerData.allowExtraEmails}
+                    color="primary"
+                  />
+                }
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    value={registerData.acceptRules}
+                    color="primary"
+                  />
+                }
+                label="I accept the rules"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default RegisterPage;
