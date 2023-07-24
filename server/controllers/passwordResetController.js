4c6560b24666e6b99362177b96148c832c15b401
Filip Alberski <alberskif@gmail.com>
2023-07-24 10:35:23 +0200

    recreation of forgotPassword

---
+++ server/controllers/passwordResetController.js
@@ -0,0 +1,63 @@
+import { User } from '../models/userModel.js';
+import asyncHandler from 'express-async-handler';
+import sendEmail from '../utils/sendEmail.js';
+import crypto from 'crypto';
+import { Token } from '../models/tokenModel.js';
+import Joi from 'joi';
+
+const forgotPassword = asyncHandler(async (req, res) => {
+  try {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      res.status(400);
+      throw new Error(error.details[0].message);
+    }
+
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      res.status(404);
+      throw new Error('User not found');
+    }
+
+    let token = await Token.findOne({ userId: user._id });
+
+    if (!token) {
+      token = await Token.create({
+        userId: user._id,
+        token: crypto.randomBytes(16).toString('hex'),
+      });
+    }
+
+    const resetPasswordLink = `${process.env.CLIENT_BASE_URL}/reset-password/${user._id}/${token.token}`;
+
+    const message = `
+        <h1>You have requested a password reset</h1>
+        <p>Please go to this link to reset your password</p>
+        <a href=${resetPasswordLink} clicktracking=off>${resetPasswordLink}</a>
+        `;
+    try {
+      sendEmail({
+        email: user.email,
+        subject: 'Password Reset Request',
+        message: message,
+      });
+      res.status(200).json({ success: true, data: 'Email sent' });
+    } catch (error) {
+      res.status(500);
+      throw new Error('Email could not be sent');
+    }
+  } catch (error) {
+    res.status(401);
+    throw new Error('Problem with forgot password');
+  }
+});
+
+const resetPassword = asyncHandler(async (req, res) => {});
+
+export { forgotPassword, resetPassword };
