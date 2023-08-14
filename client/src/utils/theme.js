b919c021fc8a9df8e9515ad28cc7f3b1c2f4802e
Filip Alberski <alberskif@gmail.com>
2023-08-14 11:33:59 +0200

    change of theme

--- client/src/utils/theme.js
+++ client/src/utils/theme.js
@@ -3,26 +3,26 @@ import { createTheme } from '@mui/material';
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
-    background: {
-      default: 'darkslategray',
-      paper: '#1E1E1E',
-    },
-    primary: {
-      main: '#FFFFFF',
-    },
+    // background: {
+    //   default: 'darkslategray',
+    //   paper: '#1E1E1E',
+    // },
+    // primary: {
+    //   main: '#FFFFFF',
+    // },
   },
 });
 
 const lightTheme = createTheme({
   palette: {
     mode: 'light',
-    background: {
-      default: '#F5F5F5',
-      paper: '#FFFFFF',
-    },
-    primary: {
-      main: '#1ff',
-    },
+    // background: {
+    //   default: '#F5F5F5',
+    //   paper: '#FFFFFF',
+    // },
+    // primary: {
+    //   main: '#1ff',
+    // },
   },
 });
 
