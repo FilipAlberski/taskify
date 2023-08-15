f085900fadd69335bd9a303a2400932eccac2031
Filip <alberskif@gmail.com>
2023-08-15 21:20:14 +0200

    update of workspaceModel

--- server/models/workspaceModel.js
+++ server/models/workspaceModel.js
@@ -1,3 +1,52 @@
 import mongoose from 'mongoose';
 
-const workoutSchema = new mongoose.Schema({});
+const workspaceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+
+  channels: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Channel',
+    },
+  ],
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  deletedAt: {
+    type: Date,
+    default: null,
+  },
+
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+
+  deletedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
